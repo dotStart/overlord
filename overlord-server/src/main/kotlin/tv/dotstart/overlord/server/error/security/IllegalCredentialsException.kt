cab8ffaa42e3ef0a8c4174e330daae45dab5d543
Johannes Donath <johannesd@torchmind.com>
2020-07-04 12:15:41 +0200

    Created an exception to handle credential related errors.

---
+++ overlord-server/src/main/kotlin/tv/dotstart/overlord/server/error/security/IllegalCredentialsException.kt
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2020 Johannes Donath <johannesd@torchmind.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package tv.dotstart.overlord.server.error.security
+
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
+
+/**
+ * Notifies callers about an issue with the given set of credentials.
+ *
+ * @author [Johannes Donath](mailto:johannesd@torchmind.com)
+ * @date 04/07/2020
+ */
+@ResponseStatus(HttpStatus.FORBIDDEN)
+class IllegalCredentialsException(message: String? = null,
+                                  cause: Throwable? = null) : Exception(message, cause)
